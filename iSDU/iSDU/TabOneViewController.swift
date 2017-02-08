499a7d2f3a9aacf5c26531b52fe5c1a1c952b154
huangyz0918 <huangyz0918@gmail.com>
2017-02-08 15:32:51 +0800

    Add Functions in TabOneViewController

--- iSDU/iSDU/TabOneViewController.swift
+++ iSDU/iSDU/TabOneViewController.swift
@@ -7,29 +7,80 @@
 //
 
 import UIKit
+import CoreLocation
+import Alamofire
+import SwiftyJSON
 
-class TabOneViewController: UIViewController {
 
+class TabOneViewController: UIViewController , CLLocationManagerDelegate{
+    
+    let LocationManager:CLLocationManager = CLLocationManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        LocationManager.delegate = self
+        LocationManager.desiredAccuracy = kCLLocationAccuracyBest
+        // Do any additional setup after loading the view, typically from a nib.
+        LocationManager.requestAlwaysAuthorization()
+        LocationManager.startUpdatingLocation()
     }
-
+    
+    //实现satrtUpdatingLocation() 的回调
+    //location 改变时回调
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let location : CLLocation = locations[locations.count-1] as CLLocation
+        
+        if location.horizontalAccuracy > 0{
+            print(location.coordinate.latitude)
+            print(location.coordinate.longitude)
+            self.updateWeatherInfo(latitude:location.coordinate.latitude,longitude: location.coordinate.longitude)
+            LocationManager.stopUpdatingLocation()
+        }
+    }
+    
+    //更新天气信息
+    func updateWeatherInfo(latitude:CLLocationDegrees,longitude:CLLocationDegrees) {
+        let mParams = ["lat":latitude,"lon":longitude]
+        let mUrl = "https://api.thinkpage.cn/v3/weather/now.json?key=hqz8lpvqz0tjvcvf&location=\(latitude):\(longitude)&language=zh-Hans&unit=c"
+        
+        print(mUrl)
+        
+        Alamofire.request(mUrl, method: .get, parameters: mParams, encoding: URLEncoding.default, headers: nil).responseJSON{(response:DataResponse<Any>) in
+            switch(response.result){
+            case .success(_):
+                //                self.progressbar.stopAnimating()
+                //                self.progressbar.isHidden = true
+                if let data = response.result.value{
+                    print("Success :=====> \(response.result.value)")
+                    let json = JSON(data)
+                    
+                    let mCityName = json["results"][0]["location"]["name"].stringValue
+                    let mTemp = json["results"][0]["now"]["temperature"].stringValue
+                    let mDisc = json["results"][0]["now"]["text"].stringValue
+                    
+                    print(mCityName)
+                    print(mTemp + "°C")
+                    print(mDisc)
+                }
+                break
+            case .failure(_):
+                print("Failure :======> \(response.result.error)")
+                
+            }
+        }
+        
+    }
+    
+    
+    //错误消息的回调函数
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print(error)
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+    
 }
