d7158082054d25c550533354df5cfcc9af06186d
Murray-Lee <li5322948@icloud.com>
2017-02-17 23:50:04 +0800

    add the WeChat SDK

--- iSDU/iSDU/AppDelegate.swift
+++ iSDU/iSDU/AppDelegate.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 @UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate {
+class AppDelegate: UIResponder, UIApplicationDelegate, WXApiDelegate {
 
     var window: UIWindow?
     
@@ -17,6 +17,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        /*
+        WXApi.registerApp("wx7cee0bbc218774cd")
+        */
         buildKeyWindow()
         return true
     }
---
+++ iSDU/iSDU/WeChatSDK/WeChatSDK-Bridging-Header.h
@@ -0,0 +1,15 @@
+//
+//  WeChatSDK-Bridging-Header.h
+//  iSDU
+//
+//  Created by murray on 2017/2/17.
+//  Copyright © 2017年 Micky&Murray. All rights reserved.
+//
+
+#ifndef WeChatSDK_Bridging_Header_h
+#define WeChatSDK_Bridging_Header_h
+@import UIKit; 
+#import "WXApiObject.h"
+#import "WXApi.h"
+
+#endif /* WeChatSDK_Bridging_Header_h */
